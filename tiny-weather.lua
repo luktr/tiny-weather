058a5124b4b40eccb21805a5f103c6ff2a6425f7
Łukasz Trąbiński <lukasz@trabinski.net>
2017-05-26 00:36:45 +0200

    - changed read data from sensor

--- tiny-weather.lua
+++ tiny-weather.lua
@@ -1,12 +1,11 @@
--- Łukasz Trąbiński <lukasz@trabinski.net> 2017 ver 0.1
 bme280.init(3,4)
 alt=0
-T, P, H, QNH = bme280.read(alt)
 
 
 srv=net.createServer(net.TCP) 
 srv:listen(80,function(conn) 
-    conn:send(string.format("NodeMCU, ESP-01, tiny-weather ver 0.1 \n\r"))
+    T, P, H, QNH = bme280.read(alt)
+    conn:send(string.format("NodeMCU, ESP-01, tiny-weather, ver 0.2 \n\r"))
     conn:send(string.format("temperature C = "))
     conn:send(string.format(T/100))
     conn:send(string.format("\n\r"))
